246e194f13917de2fe4d62b3e5e6efad1bef95cb
Hosana <hosanabarceloscandido@gmail.com>
2023-10-14 17:43:53 -0300

    fix: corrige datatestid de categorias

--- src/pages/Home/Home.test.js
+++ src/pages/Home/Home.test.js
@@ -2,6 +2,7 @@ import { advertiseRoute } from 'routes';
 import Home from '.';
 import { render, screen } from '../../test-utils';
 import userEvent from '@testing-library/user-event';
+import mockCategories from 'mocks/categories';
 
 jest.mock('services/__mocks__/categories.js')
 
@@ -18,7 +19,7 @@ afterEach(() => {
 describe('Home page', () => {
     test('if categories are rendered correctly', async () => {
         render(<Home />);
-        const categories = await screen.findAllByTestId('home-categories');
+        const categories = await screen.findAllByTestId('categories');
 
         expect(categories).toHaveLength(2);
     });
@@ -30,5 +31,16 @@ describe('Home page', () => {
          userEvent.click(button);
 
          expect(mockNavigate).toHaveBeenCalledWith(`/${advertiseRoute}`);
+    });
+
+    test('Redirect to category id', async () => {
+        render(<Home />);
+        const categories = await screen.findAllByTestId('categories');
+
+        const category01 = categories[0];
+
+        userEvent.click(category01);
+
+        expect(mockNavigate).toHaveBeenCalledWith(`/categoria/${mockCategories[0].id}`)
     });
 });
