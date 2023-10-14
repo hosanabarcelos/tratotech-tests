d8852b7618eddbd4497ee8876199e19a42bca12b
Hosana <hosanabarceloscandido@gmail.com>
2023-10-14 18:16:09 -0300

    test: add casos de teste de watchers para tarefas

--- src/store/sagas/categories.test.js
+++ src/store/sagas/categories.test.js
@@ -1,6 +1,8 @@
-import { call } from 'redux-saga/effects';
-import { observarCategorias } from './categorias';
+import { call, cancel, take } from 'redux-saga/effects';
+import { categoriasSaga, observarCategorias } from './categorias';
 import categoriasService from 'services/categorias';
+import { adicionarTodasAsCategorias } from 'store/reducers/categorias';
+import { createMockTask } from '@redux-saga/testing-utils';
 
 describe('Tests to categories saga', () => {
     describe('workers', () => {
@@ -13,6 +15,26 @@ describe('Tests to categories saga', () => {
             const executedFunction = generatorFunction.next();
 
             expect(executedFunction.value).toEqual(expectFunction);
+        });
+    });
+
+    describe('watchers', () => {
+        test('execute correctly the task', () => {
+            const generatorFunction = categoriasSaga();
+            generatorFunction.next();
+            const expectFunction = take(adicionarTodasAsCategorias);
+
+            expect(generatorFunction.next().value).toEqual(expectFunction);
+        });
+
+        test('execute the task only once', () => {
+            const generatorFunction = categoriasSaga();
+            const task = createMockTask();
+            generatorFunction.next(task);
+            const cancelExpectedFunction = cancel(task.cancel());
+            generatorFunction.next();
+
+            expect(generatorFunction.next().value).toEqual(cancelExpectedFunction);
         });
     });
 });
